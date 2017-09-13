51b9ee2b91a75f0f2c4c5cb2477d02b3800a4bc5
Abhilash Pal <abhilashpal8@gmail.com>
2017-09-13 21:05:51 +0530

    Create Array4.py

---
+++ Array4.py
@@ -0,0 +1,10 @@
+L=map(int,raw_input().split())
+n=L[0]
+d=L[1]
+E=[]
+L=map(int,raw_input().split())
+sp=n-(n-d)-1#stores the key till where we rotate :""")
+for i in range(sp+1,n):
+    print L[i],
+for j in range(0,sp+1):
+    print L[j],
