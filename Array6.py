98b9fac4cda8f5dfe651d058e91d56dbc6dfe039
Abhilash Pal <abhilashpal8@gmail.com>
2017-09-14 15:22:15 +0530

    Create Array6.py

---
+++ Array6.py
@@ -0,0 +1,21 @@
+L=map(int,raw_input().split())
+n=L[0]
+m=L[1]
+M=[]
+for i in range(n+1):
+	M.append(0)
+for i in range(m):
+	L=map(int,raw_input().split())
+	a=L[0]    #lower range
+	b=L[1]    #upper range
+	k=L[2]    #change
+	M[a]+=k   #ath element stores the change from a-1th to ath element = +k
+	if(b+1<=n):
+		M[b+1]-=k   #after the range of changes, the change to the next element = -k
+x=0
+max=0
+for i in M:
+	x+=i
+	if(x>max):
+		max=x
+print max
