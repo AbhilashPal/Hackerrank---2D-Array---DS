fe3d7dd71cd47826e4eb54685ce319465ccc24d6
Abhilash Pal <abhilashpal8@gmail.com>
2017-09-14 15:24:16 +0530

    Update Array6.py

--- Array6.py
+++ Array6.py
@@ -11,11 +11,11 @@ for i in range(m):
 	k=L[2]    #change
 	M[a]+=k   #ath element stores the change from a-1th to ath element = +k
 	if(b+1<=n):
-		M[b+1]-=k   #after the range of changes, the change to the next element = -k
-x=0
-max=0
+		M[b+1]-=k   	#after the range of changes, the change to the next element = -k
+x=0	# will store the cumulative element values
+max=0	# will store the maximum value that x obtains at any point of time through the iteration
 for i in M:
 	x+=i
 	if(x>max):
-		max=x
+		max=x	# update max
 print max
