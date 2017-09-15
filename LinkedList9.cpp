1677ef607fdc3b5490c1be2b565470d79f88ae21
Abhilash Pal <abhilashpal8@gmail.com>
2017-09-15 19:49:18 +0530

    Update LinkedList9.cpp

--- LinkedList9.cpp
+++ LinkedList9.cpp
@@ -9,29 +9,29 @@
 */
 Node* MergeLists(Node* headA, Node* headB)
 {
-	if((headA==NULL)&&(headB==NULL))
+	if((headA==NULL)&&(headB==NULL)) 			//case of empty lists
 	{
 		return NULL;
 	}
-	else if(headA==NULL)
+	else if(headA==NULL)					//base case for when A is finished 
 	{
 		return headB;
 	}
-	else if(headB==NULL)
+	else if(headB==NULL)					//base case for when B is finished 
 	{
 		return headA;
 	}
-	if ((headA->data) <= (headB->data))
-	{
+	if ((headA->data) <= (headB->data))			//if A.data<B.data then it points A.next to MergedList of 
+	{							//rest of A elements and B elements
 		headA->next=MergeLists(headA->next,headB);
 	}
 	else 
 	{
-		Node* newnode=headB;
-		headB=headB->next;
-		newnode->next=headA;
-		headA = newnode; 	 
-		headA->next = MergeLists(headA->next,headB);
-	}
-	return headA;
+		Node* newnode=headB;				//if B.data<A.data, newnode will store the B node to be
+		headB=headB->next;				//inserted into the list. B is incremented.
+		newnode->next=headA;				//The node is made to point to A's node.
+		headA = newnode; 	 			//As we are returning A, A now points to A through newnode.
+		headA->next = MergeLists(headA->next,headB);	//and the process continues as A's next elements, meaning
+	}							//newnodes next elements are Mergesorted with rest of B							
+	return headA;						//head pointer returned.
 }
